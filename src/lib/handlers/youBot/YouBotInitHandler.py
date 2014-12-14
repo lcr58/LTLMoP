1c3f20111ebe9cbefaaa0b2a9f45374b91e0f22e
Lauren Ransohoff <lcr58@cornell.edu>
2014-12-14 18:49:07 -0500

    Create YouBotInitHandler.py

---
+++ src/lib/handlers/youBot/YouBotInitHandler.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+=================================================
+youBotInit.py - youBot Initialization Handler
+=================================================
+
+Initialize the proxies to access Nao modules
+"""
+import os
+import lib.handlers.handlerTemplates as handlerTemplates
+
+class youBotInitHandler(handlerTemplates.InitHandler):
+    def __init__(self, executor, listenhost="10.0.0.128",listenport=11311):
+	"""
+        initialize youbot
+        listenhost (string) ip address of youbot
+        listenport (int) port of youbot (default=11311)
+	"""
+	os.putenv('ROS_MASTER_URI','http://{}:{}'.format(listenhost,listenport))
+    
+    def getSharedData(self):
+        return {'YOUBOT_INIT_HANDLER': self}
+
+if __name__ == "__main__":
+    pass
+    #h = youBotInitHandler(None, "10.0.0.128", port=11311)
