56e95fbb7db8f3e3922251e604d0c1e6f38b7678
Lauren Ransohoff <lcr58@cornell.edu>
2014-12-19 12:20:16 -0500

    added ROS_HOSTNAME env variable set

--- src/lib/handlers/youBot/YouBotInitHandler.py
+++ src/lib/handlers/youBot/YouBotInitHandler.py
@@ -10,13 +10,15 @@ import os
 import lib.handlers.handlerTemplates as handlerTemplates
 
 class youBotInitHandler(handlerTemplates.InitHandler):
-    def __init__(self, executor, listenhost="10.0.0.128",listenport=11311):
+    def __init__(self, executor,talkhost="10.0.0.117",listenhost="10.0.0.128",listenport=11311):
 	"""
         initialize youbot
+        talkhost (string) ip address of BEE01
         listenhost (string) ip address of youbot
         listenport (int) port of youbot (default=11311)
 	"""
 	#set the environmental variable ROS_MASTER_URI to enable connection between the LTLMoP computer and the youBot onboard PC
+	os.putenv('ROS_HOSTNAME','{}'.format(talkhost))
 	os.putenv('ROS_MASTER_URI','http://{}:{}'.format(listenhost,listenport))
     
     def getSharedData(self):
